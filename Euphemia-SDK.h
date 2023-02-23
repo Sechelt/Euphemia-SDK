3c19d9c37c7e1b5cd120c38e950103e2df7b0bed
Peter Harvey <pharvey@peterharvey.org>
2023-02-22 21:57:09 -0800

    - first release

--- Euphemia-SDK.h
+++ Euphemia-SDK.h
@@ -2,13 +2,13 @@
 #define H_Euphemia_SDK
 
     #define SDK_NAME "Euphemia-SDK"
-    #define SDK_VER "0.0.1"
-    #define SDK_VER_MAJOUR 0
+    #define SDK_VER "1.0.1"
+    #define SDK_VER_MAJOUR 1
     #define SDK_VER_MINOR 0
     #define SDK_VER_RELEASE 1
     #define SDK_LICENSE "LGPL v2.1"
     #define SDK_COPYRIGHT "Copyright 2000-2023 CodeByDesign"
     #define SDK_ORG "CodeByDesign"
-    #define SDK_DOC_VER "0.1"
+    #define SDK_DOC_VER "1.1"
 
 #endif
